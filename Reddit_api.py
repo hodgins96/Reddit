3192b6d801bda25873c6c5f831e0835351068040
hodgins96 <hodgins016@gmail.com>
2020-08-09 15:05:20 -0400

    added html email support with thumbnails, added date posted, changed post text to limit first 100 characters

--- Reddit_api.py
+++ Reddit_api.py
@@ -3,6 +3,7 @@ import json
 from email.message import EmailMessage
 import smtplib
 import os #environment variables
+from datetime import datetime
 #func get_token
 b_url = 'https://www.reddit.com/'
 reddit_name = os.environ.get('REDDIT_NAME')
@@ -23,8 +24,8 @@ token = d['access_token']
 #func call api / dissect api 
 db_url = 'https://oauth.reddit.com/r/' #url called once token is recieved
 option = '/search' #for future so can be easily changed
-subreddit = 'aww'
-search_term = 'Doggos'
+subreddit = 'BuyCanadian'
+search_term = 'Beer'
 headers = {'Authorization': 'bearer ' + token, 'User-Agent': 'subreddit_search by vaxxine_search'}
 search = {'q':search_term, 'limit': 100,'restrict_sr': True} #restric_sr restricts to that subreddit
 req = requests.get(db_url + subreddit + option, headers=headers, params=search)
@@ -36,9 +37,11 @@ siblingPosts = results['data']['children'] #how many posts are found
 # results = results['data']['children'][0]['data']
 # title = results['title']
 print(json.dumps(results, indent=4, sort_keys=True))
-title = []
+title = [] #Refactor into Dictionary????
 link = []
 selfText = []
+date = []
+thumbnail = []
 #keys = siblingPosts[0]['data'].keys()
 #print(keys)
 # print(title)
@@ -52,19 +55,25 @@ for x in siblingPosts: # for loop for searching through multiple posts
         title.append(siblingPosts[pos]['data']['title'])
         link.append(siblingPosts[pos]['data']['permalink'])
         if(siblingPosts[pos]['data']['selftext'])=="":
-            selfText.append('False')
+            selfText.append('None')
         else:
-            #selfText.append(siblingPosts[pos]['data']['selftext'])
-            selfText.append('True')
+            first25 = siblingPosts[pos]['data']['selftext']
+            if(len(first25) > 100 ):
+                selfText.append(first25[:100]+'... Continued through link') #first one hundred characters otherwise some posts are massive
+            else:
+                selfText.append(first25)
+            #selfText.append('True')
+        date.append(datetime.utcfromtimestamp(siblingPosts[pos]['data']['created_utc']))
+        thumbnail.append(siblingPosts[pos]['data']['thumbnail'])
             
-        #print("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~")
     pos+=1  
+print(date[0])
 count = 0
 content = 'Results returned from search \n\n'
 htmlContent = '<html> \n <h1>Results returned from search </h1> \n <body> \n <p> \n '
 for ele in title:
-    content = content + ('Post:' + str(count + 1) +" "+ ele +"\nLink: "+b_url +link[count]  + "\n" + "Post Text: " + selfText[count] + "\n\n")
-    htmlContent = htmlContent + ('Post:' + str(count + 1) +' '+ ele +'\nLink: '+b_url + link[count]  + '\n' + 'Post Text: ' + selfText[count] + '\n\n')
+    content = content + ('Post:' + str(count + 1) +" "+ ele +"\nLink: "+b_url +link[count]  + "\n" + "Time Posted: " + str(date[count]) + " utc \n" "Post Text: " + selfText[count] + "\n\n")
+    htmlContent = htmlContent + ('<h3>Post: ' + str(count + 1) +' '+ ele +'</h3>Link: '+b_url + link[count]  + '<br>'+"Time Posted: "+ str(date[count])+ ' utc <br>' + 'Post Text: ' + selfText[count]  + '<br>' + '<img src="' + thumbnail[count] + '"alt="Thumbnail Not Available" class="center"/> <br><br>')
     count +=1
 #func send email 
 htmlContent = htmlContent + "</p> \n </body> \n </html>"
@@ -73,14 +82,14 @@ to = 'redditsearch.bot@gmail.com'
 sender = os.environ.get('EMAIL_ACC')
 #Environment variable
 pwd = os.environ.get('EMAIL_PASS')
-print(htmlContent)
+
 msg = EmailMessage()
 msg['Subject'] = f'Search Results for {search_term} in /r/{subreddit}' #fstring for ease
 msg['From'] = sender
 msg['To'] = to
 
 msg.set_content(content) #Make email content message above in for loop
-
+msg.add_alternative(htmlContent,subtype='html')
 try:
     server = smtplib.SMTP_SSL('smtp.gmail.com', 465)
     server.login(sender,pwd)
@@ -96,7 +105,6 @@ except Exception as e:
 
 # TO DO 
 # Comment code
-# Figure out why results from other subreddits show up 
-# Email returned information 
+# Python Flags, Regex?, Clean up and refactor code 
 # Allow for custom inputs of subreddits and search terms
 
