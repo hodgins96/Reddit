99e228f4d60d1a55b03368c43ea3efad406c196c
hodgins96 <hodgins016@gmail.com>
2020-08-10 22:26:45 -0400

    refactored to list of dictionaries instead of individual lists

--- Reddit_api.py
+++ Reddit_api.py
@@ -4,7 +4,8 @@ from email.message import EmailMessage
 import smtplib
 import os #environment variables
 from datetime import datetime
-#func get_token
+#Use arg parse for flags
+#func get_token returns token
 b_url = 'https://www.reddit.com/'
 reddit_name = os.environ.get('REDDIT_NAME')
 reddit_pass = os.environ.get('REDDIT_PASS')
@@ -17,11 +18,11 @@ r = requests.post(b_url + 'api/v1/access_token',
                   data=data,
                   headers={'user-agent': 'subreddit_search by vaxxine_search'},
 		          auth=auth)
-d = r.json()
+access_token = r.json()
 
-token = d['access_token']
+token = access_token['access_token']
  
-#func call api / dissect api 
+#func call api / dissect api needs args and token
 db_url = 'https://oauth.reddit.com/r/' #url called once token is recieved
 option = '/search' #for future so can be easily changed
 subreddit = 'BuyCanadian'
@@ -33,49 +34,29 @@ req = requests.get(db_url + subreddit + option, headers=headers, params=search)
 results = req.json()
 
 siblingPosts = results['data']['children'] #how many posts are found
-#print(json.dumps(results, indent=4, sort_keys=True))
-# results = results['data']['children'][0]['data']
-# title = results['title']
 print(json.dumps(results, indent=4, sort_keys=True))
-title = [] #Refactor into Dictionary????
-link = []
-selfText = []
-date = []
-thumbnail = []
-#keys = siblingPosts[0]['data'].keys()
-#print(keys)
-# print(title)
+filtered_results = []
 pos = 0
 for x in siblingPosts: # for loop for searching through multiple posts
-    #print(siblingPosts[pos]['data']['title'])
-    #print(siblingPosts[pos]['data']['permalink'])
-    #print(siblingPosts[pos]['data']['selftext'])
     if search_term.casefold() in siblingPosts[pos]['data']['title'].casefold(): #casefold to check for work matching not case
-
-        title.append(siblingPosts[pos]['data']['title'])
-        link.append(siblingPosts[pos]['data']['permalink'])
         if(siblingPosts[pos]['data']['selftext'])=="":
-            selfText.append('None')
+            dict ={'Title':siblingPosts[pos]['data']['title'],'Link':siblingPosts[pos]['data']['permalink'],'Selftext':'No Post Body','Date':datetime.utcfromtimestamp(siblingPosts[pos]['data']['created_utc']),'Thumbnail':siblingPosts[pos]['data']['thumbnail']}
+            filtered_results.append(dict)
         else:
-            first25 = siblingPosts[pos]['data']['selftext']
-            if(len(first25) > 100 ):
-                selfText.append(first25[:100]+'... Continued through link') #first one hundred characters otherwise some posts are massive
-            else:
-                selfText.append(first25)
-            #selfText.append('True')
-        date.append(datetime.utcfromtimestamp(siblingPosts[pos]['data']['created_utc']))
-        thumbnail.append(siblingPosts[pos]['data']['thumbnail'])
+            dict ={'Title':siblingPosts[pos]['data']['title'],'Link':siblingPosts[pos]['data']['permalink'],'Selftext':siblingPosts[pos]['data']['selftext'],'Date':datetime.utcfromtimestamp(siblingPosts[pos]['data']['created_utc']),'Thumbnail':siblingPosts[pos]['data']['thumbnail']}
+            filtered_results.append(dict)
             
-    pos+=1  
-print(date[0])
+    pos+=1 
+#func send email takes in list of dictionary
+
 count = 0
 content = 'Results returned from search \n\n'
 htmlContent = '<html> \n <h1>Results returned from search </h1> \n <body> \n <p> \n '
-for ele in title:
-    content = content + ('Post:' + str(count + 1) +" "+ ele +"\nLink: "+b_url +link[count]  + "\n" + "Time Posted: " + str(date[count]) + " utc \n" "Post Text: " + selfText[count] + "\n\n")
-    htmlContent = htmlContent + ('<h3>Post: ' + str(count + 1) +' '+ ele +'</h3>Link: '+b_url + link[count]  + '<br>'+"Time Posted: "+ str(date[count])+ ' utc <br>' + 'Post Text: ' + selfText[count]  + '<br>' + '<img src="' + thumbnail[count] + '"alt="Thumbnail Not Available" class="center"/> <br><br>')
+for ele in filtered_results:
+    content = content + ('Post:' + str(count + 1) +" "+ ele['Title'] +"\nLink: "+b_url + ele['Link']  + "\n" + "Time Posted: " + str(ele['Date']) + " utc \n" "Post Text: " + ele['Selftext'] + "\n\n")
+    htmlContent = htmlContent + ('<h3>Post: ' + str(count + 1) +' '+ ele['Title'] +'</h3><b>Link:</b> '+b_url + ele['Link']  + '<br>'+"<b>Time Posted: </b>"+ str(ele['Date']) + ' utc <br>' + '<b>Post Text: </b>' + ele['Selftext']  + '<br>' + '<img src="' + ele['Thumbnail'] + '"alt="Thumbnail Not Available" class="center"/> <br><br>')
     count +=1
-#func send email 
+
 htmlContent = htmlContent + "</p> \n </body> \n </html>"
 to = 'redditsearch.bot@gmail.com'
 #environment Variable
@@ -100,11 +81,10 @@ except Exception as e:
     print(e)
 
 
-
+#if __name__ == '__main__': if imported to another class
 
 
 # TO DO 
 # Comment code
-# Python Flags, Regex?, Clean up and refactor code 
-# Allow for custom inputs of subreddits and search terms
+# Python Flags(Arg_parse), Clean up and refactor code 
 
